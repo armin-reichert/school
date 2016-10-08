75af89c5d7a7321e0eb19717580e3421ecb03b54
Armin Reichert <armin.reichert@web.de>
2016-10-08 14:07:37 +0200

    avoid warning

--- QuadGleichnung/src/de/amr/schule/quadgleichung/QuadGleichung.java
+++ QuadGleichnung/src/de/amr/schule/quadgleichung/QuadGleichung.java
@@ -1,33 +1,37 @@
 package de.amr.schule.quadgleichung;
 
+import static java.lang.System.in;
+import static java.lang.System.out;
+
 import java.util.Scanner;
 
 public class QuadGleichung {
 
 	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		while (true) {
-			System.out.println("Eingabe p,q:");
-			double p = scan.nextDouble();
-			double q = scan.nextDouble();
-			solve(p, q);
+		try (Scanner input = new Scanner(in)) {
+			while (true) {
+				out.println("Löse x^2 + p*x + q = 0");
+				out.println("Gib p und q ein:");
+				double p = input.nextDouble();
+				double q = input.nextDouble();
+				solve(p, q);
+				out.println();
+			}
 		}
 	}
 
 	public static void solve(double p, double q) {
-		System.out.println("x^2 + " + p + "x + " + q);
+		out.print("x^2 + " + p + "x + " + q);
 		double d = (-p / 2) * (-p / 2) - q;
 		if (d < 0) {
-			System.out.println("Keine Lösung");
+			out.println(" besitzt keine Lösung");
 		} else if (d == 0) {
 			double x = -p / 2;
-			System.out.println("Exakt eine Lösung: " + x);
+			out.println(" besitzt die einzige Lösung: " + x);
 		} else {
 			double x1 = -p / 2 + Math.sqrt(d);
 			double x2 = -p / 2 - Math.sqrt(d);
-			System.out.println("Erste Lösung: " + x1);
-			System.out.println("Zweite Lösung: " + x2);
+			out.println(" besitzt die Lösungen " + x1 + " und " + x2);
 		}
 	}
-
 }
