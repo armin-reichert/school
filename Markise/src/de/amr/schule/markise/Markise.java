66d0eb8421ed842aecae7c1b6626ceb26b9f4aab
Armin Reichert <armin.reichert@web.de>
2018-09-03 08:45:14 +0200

    new state machine

--- Markise/src/de/amr/schule/markise/Markise.java
+++ Markise/src/de/amr/schule/markise/Markise.java
@@ -1,5 +1,7 @@
 package de.amr.schule.markise;
 
+import static de.amr.easy.game.Application.CLOCK;
+import static de.amr.easy.game.Application.LOGGER;
 import static java.lang.String.format;
 
 import java.awt.Color;
@@ -7,19 +9,19 @@ import java.awt.Font;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
 
-import de.amr.easy.game.Application;
 import de.amr.easy.game.assets.Assets;
 import de.amr.easy.game.entity.GameEntity;
 import de.amr.easy.game.view.Controller;
 import de.amr.easy.game.view.View;
-import de.amr.easy.statemachine.StateMachine;
+import de.amr.statemachine.Match;
+import de.amr.statemachine.StateMachine;
 
 /**
  * Modell einer Markise mit Wind- und Regensensor.
  * 
  * @author Armin Reichert & Anna u. Peter Schillo
  */
-public class Markise extends GameEntity implements View,Controller {
+public class Markise extends GameEntity implements View, Controller {
 
 	private final MarkiseApp app;
 	private final Motor motor;
@@ -43,85 +45,84 @@ public class Markise extends GameEntity implements View,Controller {
 
 		// Steuerung
 
-		automat = new StateMachine<>("MarkisenSteuerung", String.class, "Eingefahren");
+		//@formatter:off
+		automat = StateMachine.define(String.class, String.class, Match.BY_EQUALITY)
+			.description("MarkisenSteuerung")
+			.initialState("Eingefahren")
 
-		// Eingefahren
+			.states()
+			
+				.state("Eingefahren")
+						.onEntry(motor::stop)
+	
+				.state("FährtAus")		
+					.onEntry(() -> {
+						motor.vor();
+						Assets.sound("bewegen.mp3").play();
+					})
+					.onTick(() -> {
+						if (position > 50 && !Assets.sound("quietschen.mp3").isRunning()) {
+							Assets.sound("quietschen.mp3").play();
+						}
+					})
+					.onExit(() -> {
+						Assets.sound("quietschen.mp3").stop();
+						Assets.sound("bewegen.mp3").stop();
+					})
+					
+				.state("Ausgefahren")
+					.onEntry(motor::stop)
+					
+				.state("FährtEin")
+					.onEntry(() -> {
+						if (regenSensor.esRegnet()) {
+							motor.schnellZurück();
+						} else {
+							motor.zurück();
+						}
+						Assets.sound("bewegen.mp3").play();
+					})
+					.onExit(() -> {
+						Assets.sound("bewegen.mp3").stop();
+					})
+					
+				.state("Gestoppt")
+					.onEntry(motor::stop)
+			
+			.transitions()
+			
+				.stay("Eingefahren").on("up")
+				.stay("Eingefahren").on("stop")
+				.when("Eingefahren").then("FährtAus").on("down").condition(() -> !regenSensor.esRegnet())
+	
+				.stay("FährtAus").on("down")
+				.when("FährtAus").then("Ausgefahren").condition(positionsSensor::inEndPosition)
+				.when("FährtAus").then("Gestoppt").on("stop")
+				.when("FährtAus").then("Gestoppt").condition(regenSensor::esRegnet)
+				.when("FährtAus").then("FährtEin").on("up")
+				
+				.stay("Ausgefahren").on("down")
+				.stay("Ausgefahren").on("stop")
+				.when("Ausgefahren").then("FährtEin").on("up")
+				.when("Ausgefahren").then("FährtEin").condition(regenSensor::esRegnet)
+				
+				.when("FährtEin").then("Eingefahren").condition(positionsSensor::inStartPosition)
+				.when("FährtEin").then("Gestoppt").on("stop")
+				.when("FährtEin").then("FährtAus").on("down")
 
-		automat.state("Eingefahren").entry = s -> motor.stop();
+				.stay("Gestoppt").on("stop")
+				.when("Gestoppt").then("FährtEin").on("up") 
+				.when("Gestoppt").then("FährtAus").on("down") 
+				.when("Gestoppt").then("FährtEin").condition(regenSensor::esRegnet)
 
-		automat.changeOnInput("down", "Eingefahren", "FährtAus", () -> !regenSensor.esRegnet());
+		.endStateMachine();
+		//@formatter:on
 
-		// FährtAus
-
-		automat.state("FährtAus").entry = s -> {
-			motor.vor();
-			Assets.sound("bewegen.mp3").play();
-		};
-
-		automat.state("FährtAus").update = s -> {
-			if (position > 50 && !Assets.sound("quietschen.mp3").isRunning()) {
-				Assets.sound("quietschen.mp3").play();
-			}
-		};
-
-		automat.state("FährtAus").exit = s -> {
-			Assets.sound("quietschen.mp3").stop();
-			Assets.sound("bewegen.mp3").stop();
-		};
-
-		automat.change("FährtAus", "Ausgefahren", positionsSensor::inEndPosition);
-
-		automat.changeOnInput("stop", "FährtAus", "Gestoppt");
-
-		automat.changeOnInput("up", "FährtAus", "FährtEin");
-
-		automat.change("FährtAus", "Gestoppt", regenSensor::esRegnet);
-
-		// Ausgefahren
-
-		automat.state("Ausgefahren").entry = s -> motor.stop();
-
-		automat.changeOnInput("up", "Ausgefahren", "FährtEin");
-
-		automat.change("Ausgefahren", "FährtEin", regenSensor::esRegnet);
-
-		// FährtEin
-
-		automat.state("FährtEin").entry = s -> {
-			if (regenSensor.esRegnet()) {
-				motor.schnellZurück();
-			} else {
-				motor.zurück();
-			}
-			Assets.sound("bewegen.mp3").play();
-		};
-
-		automat.state("FährtEin").exit = s -> {
-			Assets.sound("bewegen.mp3").stop();
-		};
-
-		automat.change("FährtEin", "Eingefahren", positionsSensor::inStartPosition);
-
-		automat.changeOnInput("stop", "FährtEin", "Gestoppt");
-
-		automat.changeOnInput("down", "FährtEin", "FährtAus");
-
-		// Gestoppt
-
-		automat.state("Gestoppt").entry = s -> motor.stop();
-
-		automat.changeOnInput("up", "Gestoppt", "FährtEin");
-
-		automat.changeOnInput("down", "Gestoppt", "FährtAus");
-
-		automat.change("Gestoppt", "FährtEin", regenSensor::esRegnet);
-
-		// Tracing
-		automat.setLogger(Application.LOGGER);
+		automat.traceTo(LOGGER, CLOCK::getFrequency);
 	}
 
 	public void raiseEvent(String event) {
-		automat.addInput(event);
+		automat.enqueue(event);
 	}
 
 	public float getPosition() {
@@ -162,7 +163,7 @@ public class Markise extends GameEntity implements View,Controller {
 		g.setFont(new Font("Monospaced", Font.BOLD, 16));
 		g.drawString(format("Wetter: %s %s  Geschw: %.1f  Position: %d%%  Zustand: %s",
 				regenSensor.esRegnet() ? "Regen" : "Sonnenschein", windSensor.esIstWindig() ? "Windig" : "Windstill",
-				tf.getVelocityX(), position, automat.stateID()), 0, 0);
+				tf.getVelocityX(), position, automat.getState()), 0, 0);
 		g.translate(-tf.getX(), -(tf.getY() + 80));
 	}
 }
