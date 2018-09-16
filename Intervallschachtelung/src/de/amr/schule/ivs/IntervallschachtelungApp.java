3774d1dc0c1265303d8d614e022f74b3388b0f4c
Armin Reichert <armin.reichert@web.de>
2018-09-16 14:27:15 +0200

    improved

--- Intervallschachtelung/src/de/amr/schule/ivs/IntervallschachtelungApp.java
+++ Intervallschachtelung/src/de/amr/schule/ivs/IntervallschachtelungApp.java
@@ -3,15 +3,15 @@ package de.amr.schule.ivs;
 public class IntervallschachtelungApp {
 
 	public static void main(String[] args) {
-		double x = hochwurzel(4, 2);
+		double x = Math.pow(4, wurzel(2));
 		System.out.println("4 hoch wurzel(2)=" + x);
 
-		double a = 2.0;
+		double a = 0;
 		long time = System.currentTimeMillis();
-		while (a < 3_000) {
+		while (a < 300_000) {
 			double wurzel = wurzel(a);
 			System.out.println(String.format("Wurzel(%g) ist ungefähr %.10g", a, wurzel));
-			a += 1;
+			a += 10;
 		}
 		long used = System.currentTimeMillis() - time;
 		System.out.println("Time[sec] needed: " + used / 1000);
@@ -22,23 +22,25 @@ public class IntervallschachtelungApp {
 		double pi, qi;
 		pi = p;
 		qi = p + delta;
-		for (int i = 0; i <= 9; i += 1) {
+		for (int i = 0; i < 10; i += 1) {
 			// System.out.println(String.format("Interval#%d [%.20g;%.20g[", i, pi, qi));
 			if (pi * pi <= a && a < qi * qi) {
-				double[] interval = new double[2];
-				interval[0] = pi;
-				interval[1] = qi;
-				return interval;
+				return new double[] { pi, qi };
 			}
 			pi = qi;
 			qi = qi + delta;
 		}
-		return null; // cannot happen?
+		throw new IllegalStateException("Kein Teilintervall enthält gesuchte Zahl"); // cannot happen?
 	}
 
 	static double wurzel(double a) {
-		double[] interval = new double[2];
-		// 1. Finde eine Quadratzahl p*p < a, nimm p als Untergrenze
+		if (a < 0) {
+			throw new IllegalArgumentException("Radikant muss größer als 0 sein");
+		}
+		if (a == 0) {
+			return 0;
+		}
+		// 1. Finde größte Quadratzahl p*p < a, nimm p als Untergrenze
 		double p = 0;
 		while (p * p < a) {
 			p += 1;
@@ -46,33 +48,10 @@ public class IntervallschachtelungApp {
 		p = p - 1;
 		// Nimm a als Obergrenze
 		double q = a;
-		interval[0] = p;
-		interval[1] = q;
+		double[] interval = new double[] { p, q };
 		for (int i = 1; i <= 10; i += 1) {
 			interval = schachtelung(a, 1, interval[0], interval[1]);
 		}
 		return interval[0];
-	}
-
-	static double hochwurzel(double b, double a) {
-		// berechne b hoch wurzel(a)
-		double[] interval = new double[2];
-
-		// 1. Finde eine Quadratzahl p*p < a, nimm p als Untergrenze
-		double p = 0;
-		while (p * p < a) {
-			p += 1;
-		}
-		p = p - 1;
-
-		// Nimm a als Obergrenze
-		double q = a;
-
-		interval[0] = p;
-		interval[1] = q;
-		for (int i = 1; i <= 10; i += 1) {
-			interval = schachtelung(a, b, interval[0], interval[1]);
-		}
-		return Math.pow(b, interval[0]);
 	}
 }
