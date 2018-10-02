f79d0780ae74d957d1db71d86064d84873d7769a
Armin Reichert <armin.reichert@web.de>
2018-10-02 14:21:27 +0200

    cleanup

--- Cryptography/src/de/amr/schule/crypto/alg/Caesar.java
+++ Cryptography/src/de/amr/schule/crypto/alg/Caesar.java
@@ -5,28 +5,30 @@ import de.amr.schule.crypto.api.MonoSubstitution;
 public class Caesar implements MonoSubstitution {
 
 	private char[] alphabet;
-	private char[] alphabetRightShift3;
-	private char[] alphabetLeftShift3;
+	private char[] alphabetRight3;
+	private char[] alphabetLeft3;
 
 	public Caesar() {
 		this("ABCDEFGHIJKLMNOPQRSTUVWXYZ .");
 	}
-	
+
 	public Caesar(String alphabetString) {
 		this.alphabet = alphabetString.toCharArray();
-		alphabetRightShift3 = alphabetString.toCharArray();
-		alphabetLeftShift3 = alphabetString.toCharArray();
+		alphabetRight3 = new char[alphabet.length];
+		alphabetLeft3 = new char[alphabet.length];
 		for (int i = 0, n = alphabet.length; i < n; ++i) {
-			alphabetLeftShift3[i] = alphabet[(i - 3 + n) % n];
-			alphabetRightShift3[i] = alphabet[(i + 3) % n];
+			alphabetLeft3[i] = alphabet[(i - 3 + n) % n];
+			alphabetRight3[i] = alphabet[(i + 3) % n];
 		}
 	}
 
+	@Override
 	public String encrypt(String text) {
-		return replace(text, c -> alphabetRightShift3[find(c, alphabet)]);
+		return replace(text, c -> alphabetRight3[find(c, alphabet)]);
 	}
 
+	@Override
 	public String decrypt(String text) {
-		return replace(text, c -> alphabetLeftShift3[find(c, alphabet)]);
+		return replace(text, c -> alphabetLeft3[find(c, alphabet)]);
 	}
 }
