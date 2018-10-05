bf5f73461c5cc2b0d4efba6fdf72afc74892ee64
Armin Reichert <armin.reichert@web.de>
2018-10-05 14:04:24 +0200

    improved

--- Cryptography/src/de/amr/schule/crypto/alg/Caesar.java
+++ Cryptography/src/de/amr/schule/crypto/alg/Caesar.java
@@ -5,27 +5,31 @@ import de.amr.schule.crypto.api.MonoSubstitution;
 public class Caesar implements MonoSubstitution {
 
 	private char[] alphabet;
-	private char[] alphabetRight3;
+	private char[] chiffre;
 
 	public Caesar() {
 		this("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz .");
 	}
 
 	public Caesar(String alphabetAsString) {
+		this(alphabetAsString, 3);
+	}
+
+	public Caesar(String alphabetAsString, int delta) {
 		this.alphabet = alphabetAsString.toCharArray();
-		alphabetRight3 = new char[alphabet.length];
+		chiffre = new char[alphabet.length];
 		for (int i = 0, n = alphabet.length; i < n; ++i) {
-			alphabetRight3[i] = alphabet[(i + 3) % n];
+			chiffre[i] = alphabet[(i + delta) % n];
 		}
 	}
 
 	@Override
 	public String encrypt(String text) {
-		return replace(text, c -> alphabetRight3[index(c, alphabet)]);
+		return replace(text, c -> chiffre[index(c, alphabet)]);
 	}
 
 	@Override
 	public String decrypt(String text) {
-		return replace(text, c -> alphabet[index(c, alphabetRight3)]);
+		return replace(text, c -> alphabet[index(c, chiffre)]);
 	}
 }
