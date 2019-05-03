b75e0d5468ea1818cd32f1ee05eff2c48244484b
Armin Reichert <armin.reichert@web.de>
2019-05-03 07:25:18 +0200

    simplify

--- WikipediaMazeSample/src/org/wikipedia/maze/Direction.java
+++ WikipediaMazeSample/src/org/wikipedia/maze/Direction.java
@@ -5,7 +5,7 @@ public enum Direction {
 	NORTH(0, -1), EAST(1, 0), SOUTH(0, 1), WEST(-1, 0);
 
 	public Direction opposite() {
-		return Direction.values()[(ordinal() + 2) % 4];
+		return values()[(ordinal() + 2) % 4];
 	}
 
 	public final int x;
