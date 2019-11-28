399e60ad400c60a1a8a4c110dadd38d5647fd2ca
Armin Reichert <armin.reichert@web.de>
2019-11-28 22:37:09 +0100

    remove static

--- Integral/src/main/java/de/amr/schule/integral/IntegralApp.java
+++ Integral/src/main/java/de/amr/schule/integral/IntegralApp.java
@@ -4,20 +4,23 @@ import java.util.function.Function;
 
 public class IntegralApp {
 
-	static double time;
-	static String fmt = "%-10s: %18.18g, expected=%18.18g, diff=%18.18g, seconds=%.2g";
+	String fmt = "%-10s: %18.18g, expected=%18.18g, diff=%18.18g, seconds=%.2g";
+	double time;
 
-	static Function<Double, Double> circle = x -> 4 * Math.sqrt(1 - x * x);
-	static Function<Double, Double> const1 = x -> 1.0;
-	static Function<Double, Double> linear = x -> x;
-	static Function<Double, Double> quadrat = x -> x * x;
-	static Function<Double, Double> sinus = x -> Math.sin(x);
-	static Function<Double, Double> exp = x -> Math.exp(x);
-	static Function<Double, Double> wand = x -> (0 <= x && x <= 3) ? x * x : Math.sqrt(x);
-	static Function<Double, Double> ln = x -> Math.log(x);
+	Function<Double, Double> circle = x -> 4 * Math.sqrt(1 - x * x);
+	Function<Double, Double> const1 = x -> 1.0;
+	Function<Double, Double> linear = x -> x;
+	Function<Double, Double> quadrat = x -> x * x;
+	Function<Double, Double> sinus = x -> Math.sin(x);
+	Function<Double, Double> exp = x -> Math.exp(x);
+	Function<Double, Double> wand = x -> (0 <= x && x <= 3) ? x * x : Math.sqrt(x);
+	Function<Double, Double> ln = x -> Math.log(x);
 
 	public static void main(String[] args) {
+		new IntegralApp().execute();
+	}
 
+	void execute() {
 		Function<Double, Double> f = ln;
 		double a = 1.0;
 		double b = 2.0;
@@ -43,19 +46,19 @@ public class IntegralApp {
 		print("Simpson2", expected, area, time);
 	}
 
-	static void startClock() {
+	void startClock() {
 		time = System.nanoTime();
 	}
 
-	static void stopClock() {
+	void stopClock() {
 		time = (System.nanoTime() - time) / 1_000_000_000L;
 	}
 
-	static void print(String name, double expected, double value, double time) {
+	void print(String name, double expected, double value, double time) {
 		System.out.println(String.format(fmt, name, value, expected, Math.abs(value - expected), time));
 	}
 
-	static int pow2(int n) {
+	int pow2(int n) {
 		return 1 << n;
 	}
 }
