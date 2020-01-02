e10836bb66e73b817137395a99d78e9c49311979
Armin Reichert <armin.reichert@web.de>
2020-01-02 07:25:40 +0100

    adapt

--- GameOfLife/src/main/java/de/amr/schule/gameoflife/scenes/GameOfLifeScene.java
+++ GameOfLife/src/main/java/de/amr/schule/gameoflife/scenes/GameOfLifeScene.java
@@ -9,11 +9,11 @@ import java.awt.Font;
 import java.awt.Graphics2D;
 
 import de.amr.easy.game.controller.Lifecycle;
-import de.amr.easy.game.view.View;
+import de.amr.easy.game.entity.Entity;
 import de.amr.schule.gameoflife.GameOfLifeApp;
 import de.amr.schule.gameoflife.GameOfLifeWorld;
 
-public class GameOfLifeScene implements View, Lifecycle {
+public class GameOfLifeScene extends Entity implements Lifecycle {
 
 	private static final int MIN_SIZE = 16;
 	private static final int MAX_SIZE = 512;
@@ -54,8 +54,7 @@ public class GameOfLifeScene implements View, Lifecycle {
 			world.setGridSize(2 * world.getGridSize());
 			world.setCellSize(getWidth() / world.getGridSize());
 			reset();
-		}
-		else if (keyPressedOnce(VK_MINUS) && world.getGridSize() / 2 >= MIN_SIZE) {
+		} else if (keyPressedOnce(VK_MINUS) && world.getGridSize() / 2 >= MIN_SIZE) {
 			world.setGridSize(world.getGridSize() / 2);
 			world.setCellSize(getWidth() / world.getGridSize());
 			reset();
