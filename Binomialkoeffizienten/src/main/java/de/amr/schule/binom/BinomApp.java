881ee75f9dc6087ba3e4bbc08109b00ad70ccfa2
Armin Reichert <armin.reichert@web.de>
2020-03-20 11:28:55 +0100

    fix

--- Binomialkoeffizienten/src/main/java/de/amr/schule/binom/BinomApp.java
+++ Binomialkoeffizienten/src/main/java/de/amr/schule/binom/BinomApp.java
@@ -3,15 +3,15 @@ package de.amr.schule.binom;
 public class BinomApp {
 
 	static final int MAX = 20;
-	static final int[][] BK = new int[MAX + 1][MAX + 1];
+	static final int[][] B = new int[MAX + 1][MAX + 1];
 	static {
-		for (int i = 0; i <= MAX; ++i) {
-			BK[i][0] = 1;
-			BK[i][i] = 1;
+		for (int n = 0; n <= MAX; ++n) {
+			B[n][0] = 1;
+			B[n][n] = 1;
 		}
-		for (int i = 2; i <= MAX; ++i) {
-			for (int j = 1; j <= MAX - 1; ++j) {
-				BK[i][j] = BK[i - 1][j - 1] + BK[i - 1][j];
+		for (int n = 2; n <= MAX; ++n) {
+			for (int k = 1; k <= n - 1; ++k) {
+				B[n][k] = B[n - 1][k - 1] + B[n - 1][k];
 			}
 		}
 	}
@@ -27,7 +27,7 @@ public class BinomApp {
 
 	static int over(int n, int k) {
 		if (0 <= n && n <= MAX && 0 <= k && k <= n) {
-			return BK[n][k];
+			return B[n][k];
 		}
 		throw new IllegalArgumentException();
 	}
