1885ac32864e09fd3f175349e34f61ea75780197
Armin Reichert <armin.reichert@web.de>
2020-03-20 14:35:09 +0100

    improved

--- Binomialkoeffizienten/src/main/java/de/amr/schule/binom/BinomApp.java
+++ Binomialkoeffizienten/src/main/java/de/amr/schule/binom/BinomApp.java
@@ -7,11 +7,12 @@ public class BinomApp {
 		app.printAll();
 	}
 
-	final int N = 20;
-	final int[][] B = new int[N + 1][N + 1];
+	static final int MAX_N = 20;
+	static final int[][] B = new int[MAX_N + 1][];
 
 	BinomApp() {
-		for (int n = 0; n <= N; ++n) {
+		for (int n = 0; n <= MAX_N; ++n) {
+			B[n] = new int[n + 1];
 			B[n][0] = B[n][n] = 1;
 			for (int k = 1; k <= n - 1; ++k) {
 				B[n][k] = B[n - 1][k - 1] + B[n - 1][k];
@@ -20,14 +21,14 @@ public class BinomApp {
 	}
 
 	int over(int n, int k) {
-		if (0 <= n && n <= N && 0 <= k && k <= n) {
+		if (0 <= n && n <= MAX_N && 0 <= k && k <= n) {
 			return B[n][k];
 		}
 		throw new IllegalArgumentException();
 	}
 
 	void printAll() {
-		for (int n = 0; n <= N; ++n) {
+		for (int n = 0; n <= MAX_N; ++n) {
 			for (int k = 0; k <= n; ++k) {
 				System.out.print(over(n, k) + " ");
 			}
