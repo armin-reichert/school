c959419705d489391faccb7c71ab4bf0e93e9c17
Armin Reichert <armin.reichert@web.de>
2020-03-20 11:41:33 +0100

    simplified

--- Binomialkoeffizienten/src/main/java/de/amr/schule/binom/BinomApp.java
+++ Binomialkoeffizienten/src/main/java/de/amr/schule/binom/BinomApp.java
@@ -2,14 +2,12 @@ package de.amr.schule.binom;
 
 public class BinomApp {
 
-	static final int MAX = 20;
-	static final int[][] B = new int[MAX + 1][MAX + 1];
+	static final int N = 25;
+	static final int[][] B = new int[N + 1][N + 1];
+
 	static {
-		for (int n = 0; n <= MAX; ++n) {
-			B[n][0] = 1;
-			B[n][n] = 1;
-		}
-		for (int n = 2; n <= MAX; ++n) {
+		for (int n = 0; n <= N; ++n) {
+			B[n][0] = B[n][n] = 1;
 			for (int k = 1; k <= n - 1; ++k) {
 				B[n][k] = B[n - 1][k - 1] + B[n - 1][k];
 			}
@@ -17,7 +15,7 @@ public class BinomApp {
 	}
 
 	public static void main(String[] args) {
-		for (int n = 0; n <= MAX; ++n) {
+		for (int n = 0; n <= N; ++n) {
 			for (int k = 0; k <= n; ++k) {
 				System.out.print(over(n, k) + " ");
 			}
@@ -26,7 +24,7 @@ public class BinomApp {
 	}
 
 	static int over(int n, int k) {
-		if (0 <= n && n <= MAX && 0 <= k && k <= n) {
+		if (0 <= n && n <= N && 0 <= k && k <= n) {
 			return B[n][k];
 		}
 		throw new IllegalArgumentException();
