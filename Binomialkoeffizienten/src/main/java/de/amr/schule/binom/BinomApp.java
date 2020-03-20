cd4e1264340d55152f2f38df7c20b2ba918776d1
Armin Reichert <armin.reichert@web.de>
2020-03-20 14:15:38 +0100

    remove static

--- Binomialkoeffizienten/src/main/java/de/amr/schule/binom/BinomApp.java
+++ Binomialkoeffizienten/src/main/java/de/amr/schule/binom/BinomApp.java
@@ -2,10 +2,15 @@ package de.amr.schule.binom;
 
 public class BinomApp {
 
-	static final int N = 25;
-	static final int[][] B = new int[N + 1][N + 1];
+	public static void main(String[] args) {
+		BinomApp app = new BinomApp();
+		app.printAll();
+	}
 
-	static {
+	final int N = 20;
+	final int[][] B = new int[N + 1][N + 1];
+
+	BinomApp() {
 		for (int n = 0; n <= N; ++n) {
 			B[n][0] = B[n][n] = 1;
 			for (int k = 1; k <= n - 1; ++k) {
@@ -14,19 +19,19 @@ public class BinomApp {
 		}
 	}
 
-	public static void main(String[] args) {
+	int over(int n, int k) {
+		if (0 <= n && n <= N && 0 <= k && k <= n) {
+			return B[n][k];
+		}
+		throw new IllegalArgumentException();
+	}
+
+	void printAll() {
 		for (int n = 0; n <= N; ++n) {
 			for (int k = 0; k <= n; ++k) {
 				System.out.print(over(n, k) + " ");
 			}
 			System.out.println();
 		}
-	}
-
-	static int over(int n, int k) {
-		if (0 <= n && n <= N && 0 <= k && k <= n) {
-			return B[n][k];
-		}
-		throw new IllegalArgumentException();
 	}
 }
